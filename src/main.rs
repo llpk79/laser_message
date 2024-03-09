503b1198e95098b4eb5056d3e9ac9af80dda8352
Paul Kutrich <pkutrich@gmail.com>
2024-03-08 22:06:57 -0800

    Longer message.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod lasers;
 mod huffman_code;
+use std::fs::read_to_string;
 
 fn main() {
-    lasers::do_laser("Send any text you like :)".to_string());
+    lasers::do_laser(read_to_string("src/lasers.rs").unwrap());
 }
